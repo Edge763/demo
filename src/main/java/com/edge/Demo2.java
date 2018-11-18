a1cdfbf446945d18cfecd4e48ddf02b46fcb4414
Edge763 <578294166@qq.com>
2018-11-18 22:08:08 +0800

    s

--- src/main/java/com/edge/Demo2.java
+++ src/main/java/com/edge/Demo2.java
@@ -2,9 +2,9 @@ package com.edge;
 
 public class Demo2 {
     public static void main(String[] args) {
-        System.out.println("fenzhi");
         System.out.println("d2");
-        System.out.println("d1");
-        System.out.println("djfie");
+        System.out.println("d2");
+        System.out.println("d2");
+        System.out.println("d2");
     }
 }
