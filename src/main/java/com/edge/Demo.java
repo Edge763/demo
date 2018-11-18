90dc973dafe7d1473710ad439471cd74a64cc92f
Edge763 <578294166@qq.com>
2018-11-18 21:36:55 +0800

    ds

--- src/main/java/com/edge/Demo.java
+++ src/main/java/com/edge/Demo.java
@@ -2,10 +2,6 @@ package com.edge;
 
 public class Demo {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("World");
-=======
-        System.out.println("ddd");
->>>>>>> f16ca1d7bbaedcf0bb7d05f0fc5b3919893b23b6
     }
 }
